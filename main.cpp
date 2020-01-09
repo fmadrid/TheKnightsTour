830e888b8229713fed404f9f0209324f4a6c38c4
Frank <fmadr002@ucr.edu>
2020-01-09 03:34:43 -0800

    Converted to std::array

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 // TO-DO: Complete the definitions of all functions other than main
 #include <iostream>     // std::cout
+#include <array>
 
 typedef unsigned int MoveType, uint;  // MoveType is aliased as data type unsigned int (i.e. MoveType variables are unsigned int variables)
 									  // uint is also aliased as unsigned int (I got tired of typing unsigned int over and over :p )
 
-typedef unsigned int ChessBoard[8][8];
+const size_t BOARD_SIZE = 8;
+
+typedef std::array<std::array<unsigned int, BOARD_SIZE>, BOARD_SIZE> ChessBoard;
 
 typedef struct Position {
 	uint currentRow;
@@ -16,11 +19,11 @@ const static int horizontalMove[8]{ -1,1,2,2,1,-1,-2,-2 };
 const static int verticalMove[8]{ -2,-2, -1,1,2,2,1,-1 };
 
 // Function Prototypes (You can change these if you know what you are doing) but these functions are really useful for this project
-void promptMove(MoveType&);
-void printBoard(const ChessBoard);
-void performMove(ChessBoard, const MoveType, const Position&, uint&);
-bool isValidMove(const ChessBoard, const MoveType, uint, uint);
-bool isFinished(const ChessBoard, const Position&, uint);
+MoveType promptMove();
+void printBoard(const ChessBoard&);
+void performMove(ChessBoard&, const MoveType&, const Position&, uint&);
+bool isValidMove(const ChessBoard&, const MoveType&, const Position&);
+bool isFinished(const ChessBoard&, const Position&, uint);
 
 // This function is done
 int main() {
@@ -40,7 +43,7 @@ int main() {
 		
 		// 4. Confirms validity of the move
 		do {move = promptMove();}
-		while (!isValidMove(board, pos, move));
+		while (!isValidMove(board, move, pos));
 
 		// 5. Perform the move
 		performMove(board, move, pos, moveCount);
@@ -57,13 +60,15 @@ MoveType promptMove() {
 	// 1. Prompt the user to enter an integer from 0 to 7
 	// 2. Validate the move (Hint: unsigned integers are already greater than 0)
 	// 3. If valid, update the move, otherwise repeat steps 1-3.
+	
+	return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 // Function printBoard
 // Iterate through the Chessboard, printing the contents of each cell
 ////////////////////////////////////////////////////////////////////////////////////////////////
-void printBoard(const ChessBoard board) {
+void printBoard(const ChessBoard& board) {
 	// Chessboard is a 2D array
 }
 
@@ -72,7 +77,7 @@ void printBoard(const ChessBoard board) {
 // Given the current state of the chessboard, determine if the the prposed move from the
 // current knight's position results in a valid position (i.e. still on the board)
 ////////////////////////////////////////////////////////////////////////////////////////////////
-bool isValidMove(const ChessBoard board, const Position& p, const MoveType m) {
+bool isValidMove(const ChessBoard& board, const MoveType& m, const Position& p) {
 	// If the move puts the knight outside of the board or onto a previously traversed space, return false; otherwise, return true
 
 	// Check if the proposed move moves the knight off the board
@@ -87,7 +92,7 @@ bool isValidMove(const ChessBoard board, const Position& p, const MoveType m) {
 // Updates the state of the chessboard by taking the proposed move and the knight's current
 // position to find the new position of the knight.
 ////////////////////////////////////////////////////////////////////////////////////////////////
-void performMove(ChessBoard board, const MoveType m, const Position& p, uint& count) {
+void performMove(ChessBoard& board, const MoveType& m, const Position& p, uint& count) {
 	
 }
 
@@ -97,7 +102,7 @@ void performMove(ChessBoard board, const MoveType m, const Position& p, uint& co
 // available moves (the knight has visited all locations we can possibly move to) or if all 64
 // squares have been visited.
 ////////////////////////////////////////////////////////////////////////////////////////////////
-bool isFinished(const ChessBoard board, const Position& p, uint moveCount) {
+bool isFinished(const ChessBoard& board, const Position& p, uint moveCount) {
 
 	// Check if all 64 squares have been visited
 
